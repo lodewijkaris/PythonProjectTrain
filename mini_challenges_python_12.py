686c1d0a8efa10fd03981d89f2416ea5c9680b86
lodewijkaris <lodewijk_aris@hotmail.com>
2024-12-03 10:03:06 +0100

    Oefeningen_python_minichallenges_mbogodigital_1-10

---
+++ mini_challenges_python_12.py
@@ -0,0 +1,18 @@
+#def repeat_name(naam, aantal_keer):
+naam = input('Wat is je naam:  ')
+aantal_keer = int(input('Hoevaak wil je je naam achter elkaar zien?: '))
+while aantal_keer != 0:
+    print((naam)*(aantal_keer))
+#    aantal_keer -= 1
+
+
+
+#print({naam}*{aantal_keer})
+
+#    for _ in range(aantal_keer):
+#        print(f'{naam}')
+#repeat_name("{naam}",aantal_keer)
+
+#naam = input('Wat is je naam:  ')
+#aantal_keer = int(input('Hoevaak wil je je naam achter elkaar zien?: '))
+#print(f'jouw naam achter elkaar "{naam}*{aantal_keer}"')
