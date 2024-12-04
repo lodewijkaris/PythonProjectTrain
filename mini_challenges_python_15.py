34fa96945e27006dbc7fb14366663db2637f748b
lodewijkaris <lodewijk_aris@hotmail.com>
2024-12-04 17:23:08 +0100

    Oefeningen_python_minichallenges_mbogodigithuiswerk2

--- mini_challenges_python_15.py
+++ mini_challenges_python_15.py
@@ -1,28 +1,23 @@
 # Input items from the user
 ingevoerde_item_sinterklaas = []
 ingevoerde_items_sinterklaas = []
-#start the while loop
-#ingevoerde_items_sinterklaas = input("Wat zou je graag van Sinterklaas willen hebben: ")#.split())
-#def append(ingevoerde_items_sinterklaas):
-#    pass
-
 while ingevoerde_item_sinterklaas != "KLAAR":
-#    ingevoerde_items_sinterklaas.append(input("Wat zou je graag van Sinterklaas willen hebben: "))
-#    ingevoerde_items_sinterklaas.append(input("Wat zou je graag van Sinterklaas willen hebben: "))
     ingevoerde_item_sinterklaas = (input("Wat zou je graag van Sinterklaas willen hebben en druk enter, type KLAAR om te stoppen: "))
     ingevoerde_items_sinterklaas.append(ingevoerde_item_sinterklaas)
     lijst = list(ingevoerde_items_sinterklaas)
-#    if ingevoerde_items_sinterklaas == "KLAAR":
-#        break
+
     print("de items die je van Sinterklaas wilt hebben:", ingevoerde_items_sinterklaas)
 
 else:    # if ingevoerde_items_sinterklaas == 'KLAAR':
-#    lijst = list(ingevoerde_items_sinterklaas)
+
     gesorteerde_reeks = sorted(lijst)
 #exclude KLAAR
     excluded_string = "KLAAR"
     filtered_list = [item for item in gesorteerde_reeks if item != excluded_string]
     print("de items die je van Sinterklaas wilt hebben gesorteerd:", filtered_list)
+
+
+#### old code
 #break
 #else:    # if ingevoerde_items_sinterklaas == 'KLAAR':
 #        exit(ingevoerde_items_sinterklaas)
@@ -40,3 +35,13 @@ else:    # if ingevoerde_items_sinterklaas == 'KLAAR':
 #items = []
 #item = list(input("Wat zou je graag van Sinterklaas willen hebben: ").split())
 #while item != ("KLAAR"):
+#start the while loop
+#ingevoerde_items_sinterklaas = input("Wat zou je graag van Sinterklaas willen hebben: ")#.split())
+#def append(ingevoerde_items_sinterklaas):
+#    pass
+
+#    ingevoerde_items_sinterklaas.append(input("Wat zou je graag van Sinterklaas willen hebben: "))
+#    ingevoerde_items_sinterklaas.append(input("Wat zou je graag van Sinterklaas willen hebben: "))
+#    if ingevoerde_items_sinterklaas == "KLAAR":
+#        break
+#    lijst = list(ingevoerde_items_sinterklaas)
