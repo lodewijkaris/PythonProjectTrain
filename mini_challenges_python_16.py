ef0fc161b5e8dec263ee2b31c8d83e6568a11af0
lodewijkaris <lodewijk_aris@hotmail.com>
2024-12-05 10:34:02 +0100

    Oefeningen_python_minichallenges_mbogodigit_huiswerk2

---
+++ mini_challenges_python_16.py
@@ -0,0 +1,6 @@
+from math import prod
+getal1 = float(input('Voer getal 1 in: '))
+getal2 = float(input('Voer getal 2 in: '))
+array = [getal1,getal2]
+product = prod(array)
+print(f'Het product van de ingevoerde getallen is {product}')
